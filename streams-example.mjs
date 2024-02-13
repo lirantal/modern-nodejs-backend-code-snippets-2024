d79a41d2b13dcc7248cb774d8c74cca0fda7fe2a
Liran Tal <liran.tal@gmail.com>
2024-02-13 11:16:45 +0200

    feat: add streams example

---
+++ streams-example.mjs
@@ -0,0 +1,41 @@
+import { ReadableStream } from "node:stream/web";
+import fs from "fs";
+
+function createReadableStreamFromFile(filePath) {
+  const stream = new ReadableStream({
+    start(controller) {
+      const reader = fs.createReadStream(filePath);
+
+      reader.on("data", (chunk) => {
+        controller.enqueue(chunk);
+        if (reader.readableFlowing === false) {
+          reader.resume();
+        }
+      });
+
+      reader.on("end", () => {
+        controller.close();
+      });
+
+      reader.on("error", (err) => {
+        controller.error(err);
+      });
+    },
+  });
+
+  return stream;
+}
+
+async function consumeStreamWithAsyncIterator(stream) {
+  try {
+    for await (const chunk of stream) {
+      process.stdout.write(chunk);
+    }
+  } catch (err) {
+    console.error("Error occurred while reading the stream:", err);
+  }
+}
+
+const filePath = process.argv[2];
+const stream = createReadableStreamFromFile(filePath);
+consumeStreamWithAsyncIterator(stream);
